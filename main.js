dba7e5953ca559ee41a2ac41d78f23be82575f90
lesavantdon <144865003+lesavantdon@users.noreply.github.com>
2023-09-14 14:59:32 -0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,28 @@
+// kelvin is 293 
+var kelvin = 293; 
+console.log(kelvin);
+
+// converting kelvin into celsius 
+ var celsius = kelvin - 20;
+ console.log(celsius);
+
+ // calculating farenheit 
+ var fahrenheit = celsius *  (9/5) + 32
+ console.log(fahrenheit);
+
+ //using floor method to round farenhiet
+ console.log(Math.floor(fahrenheit));
+var fahrenheit = 523;
+
+// using string interpolation to log temperature 
+var fahrenheit = 523;
+console.log(`the temperature is ${fahrenheit} degrees Fahrenheit.`);
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
+//whats 0 kelvin in fahrenheit 
+console.log('kelvin in 0 fahrenheit is -4 degrees fahrenehit');
+
+// converting celsius into Newton 
+var Newton = celsius * (33/100)
+console.log(Newton);
+console.log(Math.floor(Newton));
+console.log(`the temperature is ${Newton} degrees Fahrenheit.`);
